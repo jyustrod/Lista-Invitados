c9f2863ed85426900afe7b3a24a33ab7fbb77b86
jyustrod <jyustrod@myuax.com>
2025-03-04 11:06:24 +0100

    Correccion ConexionBD

--- Codigo/src/main/java/aplicacion/dao/ConexionBD.java
+++ Codigo/src/main/java/aplicacion/dao/ConexionBD.java
@@ -5,9 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:sqlserver://gestioninvitados.database.windows.net:1433;database=InvitadosBD00;" +
-            "user=UnaxDespierta@gestioninvitados;password={your_password_here};encrypt=true;" +
-            "trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+    private static final String URL = "jdbc:sqlserver://gestioninvitados.database.windows.net:1433;"
+            + "databaseName=InvitadosBD00;encrypt=true;trustServerCertificate=false;loginTimeout=30;";
     private static final String USUARIO = "UnaxDespierta";
     private static final String CONTRASEÑA = "mmFySMi5oHx2wG$Sz!$8^&Bg2";
 
