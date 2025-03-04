5b873fd0a86e608e6e6e350ea0233bea8f979c7e
jyustrod <jyustrod@myuax.com>
2025-03-04 10:02:37 +0100

    Modelo y DAO

--- Codigo/src/main/java/aplicacion/dao/InvitadoDAO.java
+++ Codigo/src/main/java/aplicacion/dao/InvitadoDAO.java
@@ -1,4 +1,24 @@
 package aplicacion.dao;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import aplicacion.model.Invitado;
+
 public class InvitadoDAO {
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/gestion_invitados", "usuario", "contraseña");
+    }
+
+    public List<Invitado> getInvitados() throws SQLException {
+        List<Invitado> lista = new ArrayList<>();
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM invitados")) {
+            while (rs.next()) {
+                lista.add(new Invitado(rs.getInt("id"), rs.getString("nombre")));
+            }
+        }
+        return lista;
+    }
 }
--- Codigo/src/main/java/aplicacion/model/Invitado.java
+++ Codigo/src/main/java/aplicacion/model/Invitado.java
@@ -1,4 +1,14 @@
 package aplicacion.model;
 
 public class Invitado {
+    private int id;
+    private String nombre;
+
+    public Invitado(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
 }
